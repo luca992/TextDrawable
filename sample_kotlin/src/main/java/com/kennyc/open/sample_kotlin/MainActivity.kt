918f6d2b44ba3e01c1c02c08bcffcb8eb1284ca4
Kennyc1012 <kennyc1012@gmail.com>
2019-06-15 07:19:34 -0400

    Update more stuff

--- sample_kotlin/src/main/java/com/kennyc/open/sample_kotlin/MainActivity.kt
+++ sample_kotlin/src/main/java/com/kennyc/open/sample_kotlin/MainActivity.kt
@@ -19,57 +19,51 @@ class MainActivity : AppCompatActivity() {
         val tv5: TextView = findViewById(R.id.test5)
         val tv6: TextView = findViewById(R.id.test6)
 
-        val d1 = TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_OVAL,
+        TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_OVAL,
                 desiredHeight = 250,
                 desiredWidth = 250,
                 color = Color.BLUE,
                 textColor = Color.RED,
                 text = "A")
+                .apply { tv1.setCompoundDrawablesWithIntrinsicBounds(this, null, null, null) }
 
-        tv1.setCompoundDrawablesWithIntrinsicBounds(d1, null, null, null)
-
-        val d2 = TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_OVAL,
+        TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_OVAL,
                 desiredWidth = 250,
                 desiredHeight = 250,
                 borderColor = Color.GREEN,
                 borderThickness = 20f,
                 color = Color.RED,
                 icon = BitmapFactory.decodeResource(resources, R.drawable.ic_action_smiley))
+                .apply { tv2.setCompoundDrawablesWithIntrinsicBounds(this, null, null, null) }
 
-        tv2.setCompoundDrawablesWithIntrinsicBounds(d2, null, null, null)
-
-        val d3 = TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_RECT,
+        TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_RECT,
                 desiredHeight = 250,
                 desiredWidth = 250,
                 color = Color.GREEN,
                 text = "A")
+                .apply { tv3.setCompoundDrawablesWithIntrinsicBounds(this, null, null, null) }
 
-        tv3.setCompoundDrawablesWithIntrinsicBounds(d3, null, null, null)
-
-        val d4 = TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_RECT,
+        TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_RECT,
                 desiredWidth = 250,
                 desiredHeight = 250,
                 color = Color.YELLOW,
                 icon = BitmapFactory.decodeResource(resources, R.drawable.ic_action_smiley))
+                .apply { tv4.setCompoundDrawablesWithIntrinsicBounds(this, null, null, null) }
 
-        tv4.setCompoundDrawablesWithIntrinsicBounds(d4, null, null, null)
-
-        val d5 = TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_ROUND_RECT,
+        TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_ROUND_RECT,
                 desiredHeight = 250,
                 desiredWidth = 250,
                 color = Color.MAGENTA,
                 cornerRadius = 50f,
                 text = "A")
+                .apply { tv5.setCompoundDrawablesWithIntrinsicBounds(this, null, null, null) }
 
-        tv5.setCompoundDrawablesWithIntrinsicBounds(d5, null, null, null)
-
-        val d6 = TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_ROUND_RECT,
+        TextDrawable(shape = TextDrawable.DRAWABLE_SHAPE_ROUND_RECT,
                 desiredWidth = 250,
                 desiredHeight = 250,
                 color = Color.LTGRAY,
                 cornerRadius = 50f,
                 icon = BitmapFactory.decodeResource(resources, R.drawable.ic_action_smiley))
-
-        tv6.setCompoundDrawablesWithIntrinsicBounds(d6, null, null, null)
+                .apply { tv6.setCompoundDrawablesWithIntrinsicBounds(this, null, null, null) }
     }
 }
